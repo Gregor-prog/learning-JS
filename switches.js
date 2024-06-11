f808cfdc70a61c2fb1b59a215add4dfbf15753dd
Gregory-prog <olajideemmanuel250@gmail.com>
2024-06-11 02:21:35 +0100

    first commit

---
+++ switches.js
@@ -0,0 +1,27 @@
+// SWITCH = Can be an efficient replacement to many else if statements
+
+ let day = 1;
+
+ switch(day){
+    case 1:
+     console.log("Today is monday");
+     break;
+     case 2:
+     console.log("today is tuesday");
+     break;
+     case 3:
+        console.log("Today is wednesday");
+        break;
+        case 4:
+            console.log("today is thursday");
+            break;
+            case5:
+            console.log("Today is friday");
+            break;
+            case 6:
+                console.log("Today is saturday");
+                break;
+
+            default: console.log("shit");
+            break  
+ }
